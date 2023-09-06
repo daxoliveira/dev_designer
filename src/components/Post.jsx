3e31a0eda508cd58c64a9c1a543a2ca367c70481
Dax Oliveira <dax.oliveira.br@gmail.com>
2023-09-06 15:09:24 -0500

    implement useState hook

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,18 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import enUS from 'date-fns/locale/en-US';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+
 import styles from './Post.module.css';
+import { useState } from 'react';
 
 export function Post({ author, publishedAt, content }) {
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ])
+
     const publishedDateFormatted = format(publishedAt, "LLLL do 'at' haaa", {
         locale: enUS
     });
@@ -12,7 +20,13 @@ export function Post({ author, publishedAt, content }) {
     const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
         locale: enUS,
         addSuffix: true
-    })
+    });
+
+    const handleCreateNewComment = () => {
+        event.preventDefault();
+
+        setComments([...comments, comments.length + 1]);
+    };
 
     return (
         <article className={styles.post}>
@@ -40,7 +54,7 @@ export function Post({ author, publishedAt, content }) {
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Leave your feedback</strong>
                 <textarea
                     placeholder='Leave your comment'
@@ -52,9 +66,9 @@ export function Post({ author, publishedAt, content }) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment />
+                })}
             </div>
         </article>
     )
